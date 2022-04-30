6462a76dda70518275b6b02015d30b27defab1b2
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-04-30 16:56:41 -0500

    Ajustando useAuth

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,16 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { userSelected } from '../constants/CommonContants';
+import UserContext from '../context/UserContext';
 
 export const useAuth = () => {
   const [logged, setLogged] = useState(false);
+  const { state } = useContext(UserContext);
 
   useEffect(() => {
-    if (localStorage.getItem(userSelected)) {
+    if (localStorage.getItem(userSelected) || state._id !== 0) {
       setLogged(true);
     } else {
       setLogged(false);
     }
-  }, []);
+  }, [state]);
 
   return logged;
 };
