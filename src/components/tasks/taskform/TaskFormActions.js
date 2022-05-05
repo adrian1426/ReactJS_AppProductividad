58d8bfa12d06dcd26f3f310424b86fe03446d7cb
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-05-04 22:45:26 -0500

    Actions controls design

--- src/components/tasks/taskform/TaskFormActions.js
+++ src/components/tasks/taskform/TaskFormActions.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { Button } from "@mui/material";
+import styles from './TaskFormActions.module.css';
 
 const TaskFormActions = () => {
   return (
-    <div>
-      Actions
+    <div className={styles.actions}>
+      <Button
+        variant='contained'
+        color='secondary'
+        fullWidth
+        className={styles.actions_button}
+      >
+        Cancelar
+      </Button>
+
+      <Button
+        variant='contained'
+        fullWidth
+        className={styles.actions_button}
+      >
+        Guardar
+      </Button>
     </div>
   );
 };
